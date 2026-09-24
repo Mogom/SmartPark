43a37c088dd8bde74bdfd55d8ccee5c9481a16fe
Juana Violeta <juanavioleta1325@gmail.com>
2026-09-23 20:06:54 -0500

    componente lading page, login

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './componentes/landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingComponent },
+  // { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '' },
+];
